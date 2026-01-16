1a7cbc5ab90cda038381d42b50e4edeed4cfa717
dome1888 <domling@hotmail.com>
2026-01-16 11:23:27 +0100

    Style

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,12 @@
 import type { Metadata } from 'next'
+import { Roboto } from 'next/font/google'
 import './globals.css'
+
+const roboto = Roboto({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+  display: 'swap',
+})
 
 export const metadata: Metadata = {
   title: 'CAS Marketing Site',
@@ -13,7 +20,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="de">
-      <body>{children}</body>
+      <body className={roboto.className}>{children}</body>
     </html>
   )
 }
